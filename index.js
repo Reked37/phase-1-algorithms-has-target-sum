6a0384c1f6e0685286e1dedb847ce409a6db4c8f
Derek Truesdale <dtruesdale37@gmail.com>
2023-05-16 15:59:51 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i=0; i<array.length;i++ ){
+    const complement = target - array[i] //difference of the first number to the target value
+    for (let j= i+1; j<array.length;j++){ //looks for the answer of the difference in the array 
+    if (array[j]=== complement) return true
+    }
+  }
+  return false
 }
 
 /* 
@@ -8,6 +15,9 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  the function must take the first number of the array and add it up to each
+  element in the array. Then compare the sum of the number to the target
+  if the sum equals the target then pass true if not then pass false
 */
 
 /*
